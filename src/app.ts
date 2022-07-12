857603f34d0fba837c0d4fddf59c839129ea19f0
ligia <ligiaarcanjo84@gmail.com>
2022-07-12 17:14:29 -0300

    feat: criada estrutura das rotas

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import productsRouter from './routers/productsRouter';
+import usersRouter from './routers/usersRouter';
+import ordersRouter from './routers/ordersRouter';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', productsRouter);
+app.use('/users', usersRouter);
+app.use('/orders', ordersRouter);
 
 export default app;
