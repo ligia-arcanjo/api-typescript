4de00ca3cc36c990a9d99980773ba0fc0786acec
ligia <ligiaarcanjo84@gmail.com>
2022-07-13 17:07:00 -0300

    feat: implementada validacoes da rota POST /products

--- src/routers/productsRouter.ts
+++ src/routers/productsRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import productsController from '../controllers/productsController';
+import { validedateName, validateAmount } from '../middlewares/productsMiddleware';
 
 const router = Router();
 
 router.get('/', productsController.getAllProducts);
-router.post('/', productsController.createProduct);
+router.post('/', validedateName, validateAmount, productsController.createProduct);
 
 export default router;
