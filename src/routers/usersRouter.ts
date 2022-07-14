8d2cdfe9ca0c78b781afe9e756fa20eb30d304df
ligia <ligiaarcanjo84@gmail.com>
2022-07-13 21:39:56 -0300

    feat: criadas validações para cadastro de usuário

--- src/routers/usersRouter.ts
+++ src/routers/usersRouter.ts
@@ -1,8 +1,19 @@
 import { Router } from 'express';
 import usersController from '../controllers/usersController';
+import { validateUsername,
+  validateClass,
+  validateLevel,
+  validatePassword } from '../middlewares/usersMiddleware';
 
 const router = Router();
 
-router.post('/', usersController.createUser);
+router.post(
+  '/',
+  validateUsername,
+  validateClass,
+  validateLevel,
+  validatePassword,
+  usersController.createUser,
+);
 
 export default router;
